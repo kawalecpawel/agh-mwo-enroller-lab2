dc9ccf3ab12afb4a37a940f99ea2c63c5f2f65b9
pkawalec <pawel.kawalec@interia.pl>
2024-04-27 11:52:11 +0200

    Mettings endpoing: get all meetings, get meeting with specific id

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -1,7 +1,9 @@
 package com.company.enroller.persistence;
 
 import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -21,5 +23,16 @@ public class MeetingService {
 		Query query = this.session.createQuery(hql);
 		return query.list();
 	}
+
+	public Meeting findById(long id) {
+		return this.session.get(Meeting.class, id);
+	}
+
+	/*public Meeting add(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(meeting);
+		transaction.commit();
+		return meeting;
+	}*/
 
 }
